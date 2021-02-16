580faa3e3d30182425141906120284726d37c689
PJZ9n <pjz9n@yahoo.co.jp>
2021-02-16 15:13:06 +0900

    メーラー設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   ActionMailer::Base.delivery_method = :letter_opener_web
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
