c51f824cb24f13c2005043befbf90590c21ae124
tatchan <tatsu.jou.0825.line2@gmail.com>
2021-02-16 23:47:07 +0900

    セキュリティのためのファイル削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_082809) do
+ActiveRecord::Schema.define(version: 2021_02_16_142648) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "text", null: false
@@ -30,6 +30,11 @@ ActiveRecord::Schema.define(version: 2021_02_16_082809) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "screen_name", null: false
     t.string "name", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmed_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
