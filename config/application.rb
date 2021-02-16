9319cd4f80c07211db9393c2defc9dacd522f2b0
PJZ9n <pjz9n@yahoo.co.jp>
2021-02-16 14:38:00 +0900

    i18n

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,10 @@ module Twitter
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :ja
   end
 end
