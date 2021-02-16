5a0d07c139d6cfb3456f21328a7f890b224d9d9c
PJZ9n <pjz9n@yahoo.co.jp>
2021-02-17 03:35:38 +0900

    ユーザーをすべてリンクに

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def user_path(user)
     '/' + user.screen_name
   end
+
+  def user_link(user)
+    link_to current_user.name + '(' + screen_name_with_at(current_user.screen_name) + ')', user_path(user)
+  end
 end
