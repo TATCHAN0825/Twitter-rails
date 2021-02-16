605045cbc00c5ce568d9d8c6d57fc86353308bcb
PJZ9n <pjz9n@yahoo.co.jp>
2021-02-16 23:04:43 +0900

    letter_openerが使用できない問題を修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: '/mail'
+  end
+
   devise_for :users
 
   resources 'tweets'
   get ':screen_name' => 'users#show'
 
   root 'home#index'
-
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: '/mail'
-  end
 end
