acef100bb5bf3e74d130f91ac823b7ff12419096
tatchan <tatsu.jou.0825.line2@gmail.com>
2021-02-18 16:16:54 +0900

    jsたぶん導入で来た

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require('bootstrap/dist/js/bootstrap.min.js')
 import 'bootstrap';
-import '@fortawesome/fontawesome-free/js/all';
+import '@fortawesome/fontawesome-free';
 import '../stylesheets/application';
 var jQuery = require('jquery')
 global.$ = global.jQuery = jQuery;
