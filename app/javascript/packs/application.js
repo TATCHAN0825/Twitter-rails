df470470676a20300f86f548470b29792b3cf6e3
PJZ9n <pjz9n@yahoo.co.jp>
2021-02-18 16:36:19 +0900

    Revert "jsたぶん導入で来た"
    This reverts commit acef100b

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require('bootstrap/dist/js/bootstrap.min.js')
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
 import 'bootstrap';
-import '@fortawesome/fontawesome-free';
+import '@fortawesome/fontawesome-free/js/all';
 import '../stylesheets/application';
 var jQuery = require('jquery')
 global.$ = global.jQuery = jQuery;
