3dff96c1857167d8ab7c0517520696122d0ac70e
PJZ9n <pjz9n@yahoo.co.jp>
2021-02-18 13:59:24 +0900

    ユーザーアイコンパス生成処理をヘルパーに切り出し

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,12 @@ module ApplicationHelper
   def user_link(user)
     link_to user.name + '(' + screen_name_with_at(user.screen_name) + ')', user_path(user)
   end
+
+  def user_image_path(user)
+    if user.image?
+      user.image.url
+    else
+      "Initialicon.png"
+    end
+  end
 end
