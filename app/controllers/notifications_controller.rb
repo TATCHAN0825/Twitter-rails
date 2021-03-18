8689ad644b1f1394b765d5ff77855930079e670d
tatchan <tatsu.jou.0825.line2@gmail.com>
2021-03-18 21:39:38 +0900

    表示まで来た ねる

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,7 +4,7 @@ class NotificationsController < ApplicationController
   def index
     @notifications = current_user.passive_notifications.page(params[:page]).per(20)
     @notifications.where(checked: false).each do |notification|
-      notification.update_attributes(checked: true)
+      notification.update_attribute(:checked, true)
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -32,14 +32,11 @@ class User < ApplicationRecord
   end
 
   def create_notification_follow!(current_user)
-    temp = notifications.where(["visitor_id = ? and visited_id = ? and action = ? ",current_user.id, id, 'follow'])
-    if temp.blank?
       notification = current_user.active_notifications.new(
         visited_id: id,
         action: 'follow'
       )
       notification.save if notification.valid?
     end
-  end
 
 end
