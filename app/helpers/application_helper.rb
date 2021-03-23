c232eb6d6b605a0ff5b385884204d69de0c32279
PJZ9n <pjz9n@yahoo.co.jp>
2021-03-23 09:10:07 +0900

    @メンションをリンクに置換する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,17 @@ module ApplicationHelper
       "Initialicon.png"
     end
   end
+
+  def replace_screen_name_to_link(text)
+    # 最初にエスケープすることでこれから追加するタグだけは残る
+    text = html_escape(text)
+
+    text.scan(/@[a-zA-Z_\d]{4,15}/).each do |screen_name|
+      screen_name.gsub!('@', '')
+      if User.find_by(screen_name: screen_name)
+        text.gsub!('@' + screen_name, link_to('@' + screen_name, user_path(screen_name: screen_name)))
+      end
+    end
+    text
+  end
 end
